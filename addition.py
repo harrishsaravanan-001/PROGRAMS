794cc5d3aeaad2d039d65eb62d3c5cd78968c539
harrishsaravanan-001 <harrishsaravanan11@gmail.com>
2026-03-11 20:29:38 +0530

    Add addition program in Python

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Python program for addition
+
+# input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# addition
+sum = num1 + num2
+
+# display result
+print("The sum is:", sum)
