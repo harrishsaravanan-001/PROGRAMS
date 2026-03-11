6955a10aec0cd68e49922919201ca4764f1580c3
harrishsaravanan-001 <harrishsaravanan11@gmail.com>
2026-03-11 20:31:10 +0530

    Add multiplication program in Python

---
+++ multiplication.py
@@ -0,0 +1,11 @@
+# Python program for multiplication
+
+# input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# multiplication
+result = num1 * num2
+
+# display result
+print("The multiplication is:", result)
