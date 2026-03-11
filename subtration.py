bc21957f347d47fc201ce63d958050e931f52b0b
harrishsaravanan-001 <harrishsaravanan11@gmail.com>
2026-03-11 20:30:22 +0530

    Add subtraction program in subtration.py

---
+++ subtration.py
@@ -0,0 +1,11 @@
+# Python program for subtraction
+
+# input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# subtraction
+result = num1 - num2
+
+# display result
+print("The subtraction is:", result)
